59f6f0bfc7151b20a7991a10e5939916ea8f28b6
LVCarnevalli <LVCarnevalli@users.noreply.github.com>
2023-09-06 10:07:42 -0300

    add comments

--- global.fixture.ts
+++ global.fixture.ts
@@ -1,6 +1,10 @@
 import { test as base } from "@playwright/test";
 import { TodoTest, TodoTestOptions } from "./pages/todo/fixture";
 
+/**
+ * Read test fixtures.
+ * https://playwright.dev/docs/test-fixtures
+ */
 interface TestOptions extends TodoTestOptions {}
 
 export const test = base.extend<TestOptions>({
--- global.setup.ts
+++ global.setup.ts
@@ -1,7 +1,7 @@
-import dotenv from "dotenv";
-
-async function globalSetup() {
-  dotenv.config({ path: ".env", override: true });
-}
+/**
+ * Read test global setup.
+ * https://playwright.dev/docs/test-global-setup-teardown#configure-globalsetup-and-globalteardown
+ */
+async function globalSetup() {}
 
 export default globalSetup;
